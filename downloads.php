0dc80881d11930107eed11d26e3c9f251578011b
Tumuhimbise Godwin <65310616+ushergodwin@users.noreply.github.com>
2024-08-02 15:27:30 +0300

    routing and downloads changes
    - improved - routing and downloads changes

--- downloads.php
+++ downloads.php
@@ -1,21 +1,32 @@
 <?php include('header.php');
 $res = dbSQL("SELECT * FROM courses order by code ");
 ?>
-    <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container text-center py-5">
-            <h3 class="text-white mb-4 animated slideInDown"> Course Outline Downloads </h3>
-        </div>
+<div class="container-fluid page-header py-1 mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container text-center py-1">
+        <h3 class="text-white mb-4 animated slideInDown">Course Outline Downloads</h3>
     </div>
+</div>
 <div class="container-xxl py-5" style="background-color: #fff;">
     <div class="container">
-        <div class="row g-5 align-items-end">
-            <div class="col-lg-12">
-                <?php foreach($res as $val):?>
-                <p> <i class="fa fa-file-pdf fa-fw"> </i><a href="uploads/<?=$val->attachment;?>" target="_blank" style="color:blue">Course Outline-<?=$val->cs_name;?> [<?=getSize(filesize("uploads/".$val->attachment));?>]</a> </p>
-            <?php endforeach;?>
+        <div class="row g-5">
+            <?php foreach ($res as $val) : ?>
+            <div class="col-lg-4 col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title"><?= $val->cs_name; ?></h5>
+                    </div>
+                    <div class="card-body">
+                        <iframe src="uploads/<?= $val->attachment; ?>" width="100%" height="200px"
+                            style="border: none;"></iframe>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="uploads/<?= $val->attachment; ?>" target="_blank" class="btn btn-primary">Download</a>
+                        <p class="mt-2">Size: <?= getSize(filesize("uploads/" . $val->attachment)); ?></p>
+                    </div>
+                </div>
             </div>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
-    <!-- About End -->
-<?php include('footer.php');?>
+</div>
+<?php include('footer.php'); ?>
--- router.php
+++ router.php
@@ -1,6 +1,7 @@
 <?php
 // Get the requested URL path
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$base = $_SERVER['DOCUMENT_ROOT'];
 
 // Serve static files directly if they exist
 $file = __DIR__ . $path;
@@ -10,6 +11,10 @@ if (file_exists($file) && !is_dir($file)) {
 
 // Remove leading slash from path
 $path = ltrim($path, '/');
+// handle calling admin login page
+if ($path === 'admin/') {
+    $path = 'admin/index';
+}
 
 // Check if the path corresponds to a PHP file without the extension
 if (file_exists(__DIR__ . '/' . $path . '.php')) {
